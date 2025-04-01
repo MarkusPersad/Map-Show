d304a9fd762deedc9f4273f1723121f3fa579ce4
MarkusPersad <msp060308@gmail.com>
2025-04-01 23:28:46 +0800

    为侧边栏添加状态激活

--- src/components/Sider.vue
+++ src/components/Sider.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="flex flex-col space-y-2 h-full bg-blue-400 dark:bg-gray-700">
-        <MapButton />
+        <MapButton :is-active="router.currentRoute.value.path === '/map'" @click="JumpTo('/map')" />
     </div>
 </template>
 <script setup lang="ts">
+import { useRouter } from "vue-router";
 import MapButton from "./Sider/MapButton.vue";
+const router = useRouter();
+const JumpTo =(path:string) =>{
+    if (router.currentRoute.value.path !== path){
+        router.push(path)
+    }
+}
 </script>
--- src/components/Sider/MapButton.vue
+++ src/components/Sider/MapButton.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="tooltip tooltip-accent tooltip-top border-0" data-tip="hello">
+    <div class="tooltip tooltip-accent tooltip-top border-0" data-tip="地图">
         <button
-            class="btn btn-square bg-blue-400 dark:bg-gray-700 iconfont border-0 shadow-none iconfont-map icontext"
+            class="btn btn-square iconfont border-0 shadow-none iconfont-map"
+            :class="{
+                ' bg-blue-700 dark:bg-gray-900 text-white':
+                    props.isActive,
+                'bg-blue-400 dark:bg-gray-700 icontext': !props.isActive,
+            }"
+            v-bind="attrs"
         ></button>
     </div>
 </template>
+<script setup lang="ts">
+import { useAttrs } from "vue";
+
+const props = defineProps<{
+    isActive: boolean;
+}>();
+const attrs = useAttrs();
+</script>
