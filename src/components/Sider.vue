7eefa71c0dcb4ab0c1bbd15a35fdd5816471d567
MarkusPersad <msp060308@gmail.com>
2025-04-02 13:36:42 +0800

    增加Settng的Sider

--- src/components/Sider.vue
+++ src/components/Sider.vue
@@ -2,12 +2,15 @@
     <div class="flex flex-col space-y-2 h-full bg-blue-400 dark:bg-gray-700">
         <MapButton :is-active="router.currentRoute.value.path === '/map'" @click="JumpTo('/map')" />
         <ChatButton :is-active="router.currentRoute.value.path === '/chat'" @click="JumpTo('/chat')" />
+        <div class="flex-grow"></div>
+        <SettingButton :is-active="router.currentRoute.value.path === '/setting'" @click="JumpTo('/setting')" />
     </div>
 </template>
 <script setup lang="ts">
 import { useRouter } from "vue-router";
 import MapButton from "./Sider/MapButton.vue";
 import ChatButton from "./Sider/ChatButton.vue"
+import SettingButton from "./Sider/SettingButton.vue";
 const router = useRouter();
 const JumpTo =(path:string) =>{
     if (router.currentRoute.value.path !== path){
